b8d6ededa847bfb36807b0e92698003d932c6e6a
Milad Irannejad <moorara@users.noreply.github.com>
2019-09-14 14:11:22 -0400

    Fix go-cover action (#32)
    * Fix go-cover action
    * Refactoring actions

--- go-cover/entrypoint.sh
+++ go-cover/entrypoint.sh
@@ -10,7 +10,7 @@ go test -race -cover -covermode=atomic -coverprofile=c.out "$*"
 exit_code=$?
 
 # Run if codeclimate_reporter_id input is set
-[ -n "$INPUT_CODECLIMATE_REPORTER_ID" ] && test-reporter after-build --id "$INPUT_CODECLIMATE_REPORTER_ID" --exit-code $exit_code
+[ -n "$INPUT_CODECLIMATE_REPORTER_ID" ] && test-reporter after-build --id "$INPUT_CODECLIMATE_REPORTER_ID" --exit-code $exit_code --prefix "$(go list -m)"
 
 # Run if codecov_token input is set
 [ -n "$INPUT_CODECOV_TOKEN" ] && bash <(curl -s https://codecov.io/bash) -f c.out -t "$INPUT_CODECOV_TOKEN"
